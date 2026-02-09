38592a4c9aab09da3bb601da7cb2dc8339325607
chelgd <chelgeorge@mail.ru>
2026-02-09 16:42:30 +0400

    Added BI to data catalog

--- src/dg_kit/base/data_catalog.py
+++ src/dg_kit/base/data_catalog.py
@@ -2,10 +2,16 @@ from __future__ import annotations
 
 
 from abc import ABC, abstractmethod
-from typing import Dict, List
+from typing import Dict, List, final
 
 from dg_kit.base.dataclasses.data_catalog import DataCatalogRow
 
+from dg_kit.base.business_information import BusinessInformation
+from dg_kit.base.dataclasses.business_information import (
+    Document,
+    Team, Contact,
+    Email, Url
+)
 from dg_kit.base.logical_model import LogicalModel
 from dg_kit.base.dataclasses.logical_model import (
     Entity,
@@ -31,11 +37,11 @@ class DataCatalogEngine(ABC):
     def pull_data_catalog(self) -> None:
         raise NotImplementedError
 
-    @abstractmethod
+    @final
     def get_row_by_id(self, id: str) -> DataCatalogRow:
         return self.rows_by_id.get(id)
 
-    @abstractmethod
+    @final
     def get_page_by_id(self, id: str) -> Entity | Attribute | Relation:
         return self.page_by_id.get(id)
 
@@ -66,22 +72,59 @@ class DataCatalog:
         self,
         LM: LogicalModel,
         PM: PhysicalModel,
+        BI: BusinessInformation,
         engine: DataCatalogEngine,
     ):
         self.LM = LM
         self.PM = PM
+        self.BI = BI
         self.engine = engine
         self.lm_objects_by_pm_id: Dict[str, List[Entity | Attribute | Relation]] = {}
-        self.pm_objects_by_lm_id: Dict[str, List[Layer | Table | Column]] = (
-            self.LM.pm_objects_by_lm_id
-        )
+        self.bi_objects_by_pm_id: Dict[str, List[Team | Contact | Document | Email | Url]] = {}
+        self.pm_objects_by_lm_id: Dict[str, List[Layer | Table | Column]] = self.LM.pm_objects_by_lm_id
+        self.bi_objects_by_lm_id: Dict[str, List[Team | Contact | Document | Email | Url]] = {}
+        self.lm_objects_by_bi_id: Dict[str, List[Entity | Attribute | Relation]] = {}
+        self.pm_objects_by_bi_id: Dict[str, List[Layer | Table | Column]] = {}
 
-        for lm_id, pm_obj in self.pm_objects_by_lm_id.items():
-            for obj in pm_obj:
-                unit_id = obj.id
-                if unit_id in self.lm_objects_by_pm_id:
-                    self.lm_objects_by_pm_id[unit_id].append(
-                        self.LM.all_units_by_id[lm_id]
-                    )
-                else:
-                    self.lm_objects_by_pm_id[unit_id] = [self.LM.all_units_by_id[lm_id]]
+        def _add_index(index: Dict[str, List], key: str, value) -> None:
+            if key in index:
+                if value not in index[key]:
+                    index[key].append(value)
+            else:
+                index[key] = [value]
+
+        # Build lm_objects_by_pm_id
+        for lm_id, pm_objs in self.pm_objects_by_lm_id.items():
+            for pm_obj in pm_objs:
+                _add_index(
+                    self.lm_objects_by_pm_id,
+                    pm_obj.id,
+                    self.LM.all_units_by_id[lm_id],
+                )
+
+        # Build bi_objects_by_lm_id and lm_objects_by_bi_id
+        for lm_id, lm_obj in self.LM.all_units_by_id.items():
+            linked_bi_units: List[Team | Contact | Document | Email | Url] = []
+
+            linked_bi_units.extend(lm_obj.responsible_parties)
+            linked_bi_units.extend(lm_obj.documents)
+
+            for team in lm_obj.responsible_parties:
+                linked_bi_units.extend(team.contacts)
+                for contact in team.contacts:
+                    linked_bi_units.extend(contact.emails)
+                    linked_bi_units.extend(contact.urls)
+
+            for bi_unit in linked_bi_units:
+                _add_index(self.bi_objects_by_lm_id, lm_id, bi_unit)
+                _add_index(self.lm_objects_by_bi_id, bi_unit.id, lm_obj)
+
+        for bi_id, lm_objs in self.lm_objects_by_bi_id.items():
+            for lm_obj in lm_objs:
+                for pm_obj in self.pm_objects_by_lm_id.get(lm_obj.id, []):
+                    _add_index(self.pm_objects_by_bi_id, bi_id, pm_obj)
+
+        for pm_id, lm_objs in self.lm_objects_by_pm_id.items():
+            for lm_obj in lm_objs:
+                for bi_unit in self.bi_objects_by_lm_id.get(lm_obj.id, []):
+                    _add_index(self.bi_objects_by_pm_id, pm_id, bi_unit)
